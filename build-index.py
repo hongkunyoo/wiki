0bd1a45ca9da9ea141b55a53fa92f998e391db9c
hongkunyoo <hongkunyoo@gmail.com>
2025-06-18 23:00:14 +0900

    test

--- build-index.py
+++ build-index.py
@@ -1,7 +1,40 @@
 import os
 
-for f in os.listdir():
-    if f == ".github" or f == ".git" or f == "index.html":
-        continue
 
-    print(f)
+HTML="""\
+<!doctype html>
+<html lang="" data-theme="light">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Personal wiki</title>
+  <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"
+>
+  <meta name="description" content="">
+</head>
+
+<body>
+  <main>
+
+    <h1>Personal Wiki page</h1>
+    <ul>
+      {0}
+    </ul>
+  </main>
+
+</body>
+</html>
+"""
+
+arr = []
+for f in os.listdir():
+    if f == ".github" or f == ".git" or f == "index.html" or f == "build-index.py":
+        continue
+    arr.append(f"<li><a target='_blank' href='{f}'>{f}</li>")
+
+    output = HTML.format("\n".join(arr))
+    with open("index.html", "w") as f:
+        print(output, file=f)
