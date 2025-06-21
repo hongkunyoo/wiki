953e94719bc4de7de380b8810a305bdc7ca2aeed
hongkunyoo <hongkunyoo@gmail.com>
2025-06-22 01:36:29 +0900

    a

--- _build-index.py
+++ _build-index.py
@@ -13,6 +13,13 @@ HTML="""\
   rel="stylesheet"
   href="_pico.classless.min.css"
 >
+  <style>
+    :root {{
+        --pico-typography-spacing-vertical: 0rem;
+
+    }}
+
+  </style>
   <meta name="description" content="">
 </head>
 
@@ -20,21 +27,39 @@ HTML="""\
   <main>
 
     <h1>Personal wiki</h1>
-    <ul>
       {0}
-    </ul>
   </main>
 
 </body>
 </html>
 """
 
-arr = []
-for f in sorted(os.listdir()):
-    if f == ".github" or f == ".git" or f == "index.html" or f == "_build-index.py" or f == "_pico.classless.min.css":
-        continue
-    arr.append(f"<li><a target='_blank' href='{f}'>{f}</li>")
+def gen_li(curdir, excepts):
+    arr = []
+    for f in sorted(os.listdir(curdir)):
+        if f in excepts:
+            continue
+        if os.path.isdir(os.path.join(curdir, f)):
+            ff = gen_li(os.path.join(curdir, f), excepts)
+            d = os.path.join(curdir, f)
+            arr.append(f"<li><a href='{d}' target='_blank'>{f}/</a> {ff}</li>")
+        else:
+            arr.append(f"<li><a target='_blank' href='{curdir}/{f}'>{f}</a></li>")
+    return "<ul>" + ("\n".join(arr)) + "</ul>"
 
-    output = HTML.format("\n      ".join(arr))
-    with open("index.html", "w") as f:
-        print(output, file=f)
+
+
+arr = []
+#for f in sorted(os.listdir()):
+#    if f == ".github" or f == ".git" or f == "index.html" or f == "_build-index.py" or f == "_pico.classless.min.css":
+#        continue
+#    if os.path.isdir(f):
+#        for ff in sorted(os.listdir(f)):
+#
+#    arr.append(f"<li><a target='_blank' href='{f}'>{f}</li>")
+
+arr = gen_li(".", [".github", ".git", "index.html", "_build-index.py", "_pico.classless.min.css"])
+
+output = HTML.format(arr)
+with open("index.html", "w") as f:
+    print(output, file=f)
