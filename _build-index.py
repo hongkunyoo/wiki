c17544513bb204524a36160a859e68793f6720e1
hongkunyoo <hongkunyoo@gmail.com>
2025-06-25 20:25:46 +0900

    a

--- _build-index.py
+++ _build-index.py
@@ -94,15 +94,6 @@ def gen_li(curdir, excepts):
 
 
 
-arr = []
-#for f in sorted(os.listdir()):
-#    if f == ".github" or f == ".git" or f == "index.html" or f == "_build-index.py" or f == "_pico.classless.min.css":
-#        continue
-#    if os.path.isdir(f):
-#        for ff in sorted(os.listdir(f)):
-#
-#    arr.append(f"<li><a target='_blank' href='{f}'>{f}</li>")
-
 arr = gen_li(".", [".github", ".git", "index.html", "_build-index.py", "_pico.classless.min.css", ".gitignore"])
 
 output = HTML.format(arr)
